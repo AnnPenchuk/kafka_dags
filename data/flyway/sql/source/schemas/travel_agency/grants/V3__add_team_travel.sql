ca6f7bba98b05133b28ff6979edcd3307555acf2
AnnPenchuk <annapenchuk95@mail.ru>
2024-12-10 12:10:48 +0300

    readme

--- data/flyway/sql/source/schemas/travel_agency/grants/V3__add_team_travel.sql
+++
@@ -1,8 +0,0 @@
-CREATE USER backend_app WITH PASSWORD 'password_backend_app';
-GRANT travel_rw TO backend_app;
-
-CREATE USER developer WITH PASSWORD 'password_developer';
-GRANT travel_r TO developer;
-
-CREATE USER analyst WITH PASSWORD 'password_analyst';
-GRANT travel_r TO analyst;
